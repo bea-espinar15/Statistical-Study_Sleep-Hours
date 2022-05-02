9ebde9f6e24eb7a87b6df5281ed122f755351b57
bea-espinar15 <beaesp01@ucm.es>
2022-05-02 14:43:07 +0200

    Refactorización

--- por_si_acaso.R
+++ por_si_acaso.R
@@ -246,3 +246,146 @@ diag.disp.GLyGEI <- ggplot(df.GLyGEI, aes(x = GL, y = GEI)) +
   ylab("")
 
 cor(vector.GL, vector.GEI)
+
+# A = GE > 1000; B = NEH = 1. ¿P(A/B)?
+# P(A n B)
+c.fav.5 <- count(df.NEHyGE %>% filter(NEH == 1 & GE > 1000))
+c.pos.5 <- count(df.NEHyGE)
+prob.5 <- c.fav.5 / c.pos.5
+# P(B)
+c.fav.6 <- count(df.NEH %>% filter(NEH == 1))
+c.pos.6 <- count(df.NEH)
+prob.6 <- c.fav.6 / c.pos.6
+# P(A/B)
+prob.7 <- prob.5 / prob.6
+
+# A = GE > 1000; B = NEH = 2. ¿P(A/B)?
+# P(A n B)
+c.fav.8 <- count(df.NEHyGE %>% filter(NEH == 2 & GE > 1000))
+c.pos.8 <- count(df.NEHyGE)
+prob.8 <- c.fav.8 / c.pos.8
+# P(B)
+c.fav.9 <- count(df.NEH %>% filter(NEH == 2))
+c.pos.9 <- count(df.NEH)
+prob.9 <- c.fav.9 / c.pos.9
+# P(A/B)
+prob.10 <- prob.8 / prob.9
+
+# A = GE > 1000; B = NEH = 3. ¿P(A/B)?
+# P(A n B)
+c.fav.11 <- count(df.NEHyGE %>% filter(NEH == 3 & GE > 1000))
+c.pos.11 <- count(df.NEHyGE)
+prob.11 <- c.fav.11 / c.pos.11
+# P(B)
+c.fav.12 <- count(df.NEH %>% filter(NEH == 3))
+c.pos.12 <- count(df.NEH)
+prob.12 <- c.fav.12 / c.pos.12
+# P(A/B)
+prob.13 <- prob.11 / prob.12
+
+# A = GE > 1000; B = NEH = 4. ¿P(A/B)?
+# P(A n B)
+c.fav.14 <- count(df.NEHyGE %>% filter(NEH == 4 & GE > 1000))
+c.pos.14 <- count(df.NEHyGE)
+prob.14 <- c.fav.14 / c.pos.14
+# P(B)
+c.fav.15 <- count(df.NEH %>% filter(NEH == 4))
+c.pos.15 <- count(df.NEH)
+prob.15 <- c.fav.15 / c.pos.15
+# P(A/B)
+prob.16 <- prob.14 / prob.15
+
+# A = GE > 1000; B = NEH = 5. ¿P(A/B)?
+# P(A n B)
+c.fav.17 <- count(df.NEHyGE %>% filter(NEH == 5 & GE > 1000))
+c.pos.17 <- count(df.NEHyGE)
+prob.17 <- c.fav.17 / c.pos.17
+# P(B)
+c.fav.18 <- count(df.NEH %>% filter(NEH == 5))
+c.pos.18 <- count(df.NEH)
+prob.18 <- c.fav.18 / c.pos.18
+# P(A/B)
+prob.19 <- prob.17 / prob.18
+
+# A = GE > 1000; B = NEH = 6. ¿P(A/B)?
+# P(A n B)
+c.fav.20 <- count(df.NEHyGE %>% filter(NEH == 6 & GE > 1000))
+c.pos.20 <- count(df.NEHyGE)
+prob.20 <- c.fav.20 / c.pos.20
+# P(B)
+c.fav.21 <- count(df.NEH %>% filter(NEH == 6))
+c.pos.21 <- count(df.NEH)
+prob.21 <- c.fav.21 / c.pos.21
+# P(A/B)
+prob.22 <- prob.20 / prob.21
+
+hist(vector.GE, freq = FALSE, xlim = c(0,60000), breaks = intervalos)
+lines(density(vector.GE))
+
+# rm todas las col exc la 2 (ID), la 4 (NPH), la 5 (NEH), la 57 (GE) y las filas que sean NA
+datos <- datos %>% select(2,4,5,57) %>% filter(!is.na(NHOGAR) & !is.na(EHOGAR) & !is.na(GTT))
+
+# pasamos NHOGAR de char a numeric
+datos$NPH <- as.numeric(datos$NHOGAR)
+datos <- datos %>% select(1,3,4,5)
+
+# pasamos EHOGAR de char a numeric
+datos$NEH <- as.numeric(datos$EHOGAR)
+datos <- datos %>% select(1,3,4,5)
+
+# rehacemos el fichero con los gastos totales por hogar 
+# (viene una entrada por cada estudiante)
+datos.NEHyGE <- datos %>% group_by(IDHOGAR, NEH) %>% summarise(GE = sum(GTT))
+datos.NEHyGE <- datos.NEHyGE %>% ungroup() %>% select(2, 3)
+
+# separamos en dos variables
+num.est.hogar <- datos.NEHyGE %>% select(1)
+gasto.ed <- datos.NEHyGE %>% select(2)
+
+# tomamos una nueva variable: número de personas por hogar
+datos.NPHyNEH2 <- datos %>% select(1,3,4)
+datos.NPHyNEH2 <- datos.NPHyNEH2 %>% group_by(IDHOGAR)
+datos.NPHyNEH2 <- datos.NPHyNEH2 %>% ungroup() %>% select(2, 3)
+
+# separamos variables
+num.per.hogar <- datos.NPHyNEH2 %>% select(1)
+num.est.hogar.2 <- datos.NPHyNEH2 %>% select(2)
+
+# pasamos a tibble
+num.per.hogar <- as_tibble(num.per.hogar)
+num.est.hogar.2 <- as_tibble(num.est.hogar.2)
+
+# calculamos vectores necesarios
+vector.NPH <- ObtenerVector(num.per.hogar[1])
+vector.NEH2 <- ObtenerVector(num.est.hogar.2[1])
+
+# NPH
+vec.NPH <- ObtenerVector(tib.NPH[1])
+
+# NPH
+tib.NPH <- as_tibble(df.NPH)
+
+# NÚMERO DE PERSONAS POR HOGAR
+
+# nos quedamos con la col 2 (ID) y la 4 (NPH) y eliminamos las filas que sean NA
+df.NPH <- datos %>% select(2,4) %>% filter(!is.na(NHOGAR))
+
+# pasamos NHOGAR de char a numeric
+df.NPH$NPH <- as.numeric(df.NPH$NHOGAR)
+df.NPH <- df.NPH %>% select(1,3)
+
+# agrupamos por hogar (hay más de una fila por hogar)
+df.NPH <- df.NPH %>% group_by(IDHOGAR)
+df.NPH <- df.NPH %>% ungroup() %>% select(2)
+
+# NPH
+n.NPH <- as.numeric(count(df.NPH))
+
+datos.NEHyGL <- fichero_salida %>% select(2,5,44) %>% filter(!is.na(EHOGAR) & !is.na(LIB))
+# pasamos EHOGAR de char a numeric
+datos.NEHyGL$NEH <- as.numeric(datos.NEHyGL$EHOGAR)
+datos.NEHyGL <- datos.NEHyGL %>% select(1,3,4)
+# rehacemos el fichero con los gastos totales por hogar
+datos.NEHyGL <- datos.NEHyGL %>% group_by(IDHOGAR, NEH) %>% summarise(GL = sum(LIB))
+datos.NEHyGL <- datos.NEHyGL %>% ungroup() %>% select(2, 3)
+
